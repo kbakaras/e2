eb51e48c233822b64a15a1b0f2212c58b428f490
kbakaras <kbakaras@gmail.com>
2019-04-22 10:26:51 +0300

    Перетащил конфигурацию из agr.

---
+++ src/main/java/ru/kbakaras/e2/DbBeanConfig.java
@@ -0,0 +1,17 @@
+package ru.kbakaras.e2;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.kbakaras.jpa.repository.SugarRepositoryExe;
+
+@Configuration
+@EnableJpaRepositories(
+        value = "ru.kbakaras.e2.repositories",
+        repositoryBaseClass = SugarRepositoryExe.class
+)
+@EntityScan(basePackages = {
+        "ru.kbakaras.e2.model",
+        "ru.glance.agr.model"
+})
+class DbBeanConfig {}
---
+++ src/main/java/ru/kbakaras/e2/Element4jHttpMessageConverter.java
@@ -0,0 +1,58 @@
+package ru.kbakaras.e2;
+
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@Component
+public class Element4jHttpMessageConverter extends AbstractHttpMessageConverter<Element> {
+	private SAXReader reader;
+
+	public Element4jHttpMessageConverter() {
+		super(MediaType.TEXT_XML, MediaType.APPLICATION_XML);
+		this.reader = new SAXReader();
+	}
+
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Element.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	protected Element readInternal(Class<? extends Element> clazz, HttpInputMessage inputMessage) throws IOException {
+		try {
+			return reader.read(inputMessage.getBody()).getRootElement();
+		} catch (DocumentException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected Long getContentLength(Element element, MediaType contentType) {
+		return null;
+		/*Charset charset = getContentTypeCharset(contentType);
+		try {
+			return (long) element.getBytes(charset.name()).length;
+		}
+		catch (UnsupportedEncodingException ex) {
+			// should not occur
+			throw new IllegalStateException(ex);
+		}*/
+	}
+
+	@Override
+	protected void writeInternal(Element element, HttpOutputMessage outputMessage) throws IOException {
+		StreamUtils.copy(element.asXML(), Charset.forName("UTF-8"), outputMessage.getBody());
+	}
+
+}
