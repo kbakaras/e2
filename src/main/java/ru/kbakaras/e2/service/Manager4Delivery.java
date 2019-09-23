5b5c33920639d9da92a005505990f0fbed55284b
kbakaras <kbakaras@gmail.com>
2019-09-23 10:44:07 +0300

    Manager4Delivery прототип.

---
+++ src/main/java/ru/kbakaras/e2/service/Manager4Delivery.java
@@ -0,0 +1,18 @@
+package ru.kbakaras.e2.service;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Manager4Delivery implements InitializingBean, DisposableBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+}
