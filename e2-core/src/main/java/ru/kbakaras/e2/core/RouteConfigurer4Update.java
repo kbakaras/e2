9ea43ef77c8d40ef8b822df01bb357f35b68a62a
Konstantin Bakaras <k.bakaras@voskhod.ru>
2019-12-24 16:07:35 +0300

    Описание запросов

--- e2-core/src/main/java/ru/kbakaras/e2/core/RouteConfigurer4Update.java
+++ e2-core/src/main/java/ru/kbakaras/e2/core/RouteConfigurer4Update.java
@@ -4,5 +4,5 @@ import ru.kbakaras.e2.core.model.SystemConnection;
 
 @FunctionalInterface
 public interface RouteConfigurer4Update {
-    void add(SystemConnection from, SystemConnection to, String...entities);
+    void add(SystemConnection from, SystemConnection to, String... entities);
 }
